d5b5fe931071746e3c357f435e71ee30ec640854
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-04 13:29:30 +0000

    3rd exercise

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count;
