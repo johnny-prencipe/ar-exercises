c19eff5033acb8df027bc3230ccb3181030ce215
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-04 14:02:11 +0000

    Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.all.sum(:annual_revenue)
+average_per_store = total_revenue / Store.all.count
+total_over_million = Store.where("annual_revenue > ?", 1000000).count
+
+print "Total revenue: "
+puts total_revenue
+print "Average per store: "
+puts average_per_store
+print "Number of stores earning over $1,000,000: "
+puts total_over_million
